0d5972c8b73bcdfb7a445e36f217660a11eff7e5
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2024-01-15 10:53:24 +0100

    First and second line
    Combo with Console.WriteLine(); and Console.Write();

--- MS Learn/Program.cs
+++ MS Learn/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-Console.Write("Congratulations!");
-Console.Write(" ");
-Console.Write("You wrote your first line of code.");
+//Console.Write("Congratulations!");
+//Console.Write(" ");
+//Console.Write("You wrote your first line of code.");
+Console.WriteLine("This is the first line.");
+Console.Write("This is the first line.");
+
 string userName = Console.ReadLine();
