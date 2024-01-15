a74cd6ed66f6c498b91f8768376037a74084a082
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2024-01-15 20:58:07 +0100

    Challenge: Format and display instructions

--- MS Learn/Program.cs
+++ MS Learn/Program.cs
@@ -2,6 +2,8 @@
 
 //Module 1
 //Showing Console.WriteLine();
+using System.Security;
+
 Console.WriteLine("Hello, World!");
 
 //showing Console.Write();/ This cmd writes everything on the same line.
@@ -70,16 +72,94 @@ Console.WriteLine(false);
 //Console.WriteLine(firstName);
 
 //Challenge: Display literal and variable values
-string firstName;
-firstName = "Bob";
-Console.Write("Hello, ");
-Console.Write(firstName);
-Console.Write("! you have ");
-Console.Write(3);
-Console.Write(" messages in your inbox. ");
-Console.Write("The temperature is ");
-Console.Write(34.4m);
-Console.Write(" celsius.");
+//string firstName;
+//firstName = "Bob";
+//Console.Write("Hello, ");
+//Console.Write(firstName);
+//Console.Write("! you have ");
+//Console.Write(3);
+//Console.Write(" messages in your inbox. ");
+//Console.Write("The temperature is ");
+//Console.Write(34.4m);
+//Console.Write(" celsius.");
+
+
+//Exercise - Combine strings using character escape sequences
+//Character escape sequences
+
+//Console.WriteLine("Hello\nWorld!");
+//Console.WriteLine("Hello\tWorld!");
+
+////Console.WriteLine("Hello "World"!");
+
+//Console.WriteLine("Hello \"world\"!");
+
+////Conosole.WriteLine("C:\Source\repos");
+
+//Console.WriteLine("c:\\source\\repos");
+
+//Console.WriteLine("Generating invoices for customer \"Contoso corp\" ...\n");
+
+//Console.WriteLine("Invoice: 1021\t\tComplete!");
+//Console.WriteLine("Invoice: 1022\t\tComplete!");
+//Console.WriteLine("\nOutput Directory:\t");
+
+//Console.WriteLine(@"    c:\source\repos    
+//        (this is where your code goes)");
+
+//Console.Write(@"c:\invoices");
+
+//// Kon'nichiwa World
+//Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
+
+//// To generate Japanese invoices:
+//// Nihon no seikyū-sho o seisei suru ni wa:
+//Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+//// User command to run an application
+//Console.WriteLine(@"c:\invoices\app.exe -j");
+
+//Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+//Console.WriteLine("Invoice: 1021\t\tComplete!");
+//Console.WriteLine("Invoice: 1022\t\tComplete!");
+//Console.WriteLine("\nOutput Directory:\t");
+//Console.Write(@"c:\invoices");
+
+//// To generate Japanese invoices:
+//// Nihon no seikyū-sho o seisei suru ni wa:
+//Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+//// User command to run an application
+//Console.WriteLine(@"c:\invoices\app.exe -j");
+
+//Exercise - Combine strings using string concatenation
+
+//Concatenate a literal string and a variable
+//string firstName = "Bob";
+//string message = "Hello " + firstName;
+//Console.WriteLine(message);
+
+//Concatenate multiple variables and literal strings
+//string firstName = "Bob";
+//string greeting = "Hello";
+//string message = greeting + " " + firstName + "!";
+//Console.WriteLine(message);
+
+//int version = 11;
+//string updateText = "Update to Windows";
+//string message = $"{updateText} {version}";
+//Console.WriteLine(message);
+
+////combined verbatim literals and string inerpolation
+//string projectName = "First-Project";
+//Console.WriteLine($@"c:\Output\{projectName}\Data");
+
+//chalange: Format and display instructions
+string projectName = "ACME";
+string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434:";
+string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
 
 
 
