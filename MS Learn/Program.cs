958eeac2558edb3856789560e5b574c842504a48
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2024-01-15 10:40:27 +0100

    New Changes
    Console.Write();

--- MS Learn/Program.cs
+++ MS Learn/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first line of code.");
 string userName = Console.ReadLine();
