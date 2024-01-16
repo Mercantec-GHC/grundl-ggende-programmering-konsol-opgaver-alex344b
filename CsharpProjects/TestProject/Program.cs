415941d8561f21bf46db9b9038d98b90b612f5a5
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2024-01-16 14:36:57 +0100

    Declaring a new array

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -104,32 +104,49 @@
 
 //Challenge: Improve renewal rate of subscriptions
 
-Random random = new Random();
-int daysUntilExperiration = random.Next(12);
-int discountPercentage = 0;
+// Random random = new Random();
+// int daysUntilExperiration = random.Next(12);
+// int discountPercentage = 0;
 
-if (daysUntilExperiration <= 10)
-{
+// if (daysUntilExperiration <= 10)
+// {
 
-    if (daysUntilExperiration <= 5 && daysUntilExperiration >= 1)
-    {
-        discountPercentage = 10;
-        Console.WriteLine($"Your subscription expires in {daysUntilExperiration} days. Renew now and save {discountPercentage}%");
-    }
-    else if (daysUntilExperiration == 1)
-    {
-        discountPercentage = 20;
-        Console.WriteLine($"Your subscription expires within a day! Renew now and save {discountPercentage}%!");
-    }
-    else if (daysUntilExperiration == 0)
-    {
-        Console.WriteLine("Your subscription has expired.");
-    }
-    else 
-    {
-        Console.WriteLine("Your subscription will expire soon. Renew now!");
-    }
-}
+//     if (daysUntilExperiration <= 5 && daysUntilExperiration >= 1)
+//     {
+//         discountPercentage = 10;
+//         Console.WriteLine($"Your subscription expires in {daysUntilExperiration} days. Renew now and save {discountPercentage}%");
+//     }
+//     else if (daysUntilExperiration == 1)
+//     {
+//         discountPercentage = 20;
+//         Console.WriteLine($"Your subscription expires within a day! Renew now and save {discountPercentage}%!");
+//     }
+//     else if (daysUntilExperiration == 0)
+//     {
+//         Console.WriteLine("Your subscription has expired.");
+//     }
+//     else 
+//     {
+//         Console.WriteLine("Your subscription will expire soon. Renew now!");
+//     }
+// }
+
+
+//Declaring a new array
+string[] fraudulentOrderIDs = new string[3];
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+//fraudulentOrderIDs[3] = "D000";
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+
+
+
+
 
 
 
