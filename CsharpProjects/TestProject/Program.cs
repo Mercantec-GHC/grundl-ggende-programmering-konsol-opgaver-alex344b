ebb8bfdeb81c66dfe10393cc05f613267f837325
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2024-01-16 14:56:36 +0100

    Create a variable to hold the current bin number and display the running total

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -142,20 +142,39 @@ fraudulentOrderIDs[2] = "C789";
 
 */
 //Initialize an array
-string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+// string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
 
-Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
-Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
 
-fraudulentOrderIDs[0] = "F000";
+// fraudulentOrderIDs[0] = "F000";
 
-Console.WriteLine($"Reassing First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
-
-
+// Console.WriteLine($"Reassing First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
 
 
+//Looping through an array using foreach
+
+// string[] names = { "Rowena", "Robin", "Bao" };
+// foreach (string name in names)
+// {
+//     Console.WriteLine(name);
+// }
+
+
+//Create a variable to hold the current bin number and display the running total
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+
+Console.WriteLine($"We have a {sum} items in inventory");
 
 
 
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+415941d8561f21bf46db9b9038d98b90b612f5a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+073b15cbded23250302188031195efc99b40a85f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
