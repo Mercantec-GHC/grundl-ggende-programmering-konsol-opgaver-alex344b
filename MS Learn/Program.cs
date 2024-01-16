15dac55f7ce6cc361582da277d86e88e97dc7364
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2024-01-16 08:14:41 +0100

    Add parentheses to clarify your intention to the compiler

--- MS Learn/Program.cs
+++ MS Learn/Program.cs
@@ -167,7 +167,7 @@ int secondNumber = 9;
 Console.WriteLine(firstNumber + secondNumber);
 string firstName = "Bob";
 int widgetsSold = 7;
-Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
 
 
 
