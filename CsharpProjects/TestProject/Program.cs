3581c353a753bd0994413b558269509a28c80952
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2024-01-16 14:02:03 +0100

    Doubles and Triples game!

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -44,16 +44,72 @@
 // Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 
-string message = "The quick brown fox jups over the lazy dog";
-bool result = message.Contains("dog");
-Console.WriteLine(result);
+// string message = "The quick brown fox jups over the lazy dog";
+// bool result = message.Contains("dog");
+// Console.WriteLine(result);
 
-if (message.Contains("fox"))
+// if (message.Contains("fox"))
+// {
+//     Console.WriteLine("What does the fox say?!");
+// }
+
+// if (message.Contains("fox"))
+// {
+//     Console.WriteLine("DING DING DING DING DING DING!");
+// }
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("What does the fox say?!");
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("you rolled triples! +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("you rolled doubles! +2 bonus to total");
+        total += 2;
+    }
 }
 
-if (message.Contains("fox"))
+if (total >= 14)
 {
-    Console.WriteLine("DING DING DING DING DING DING!");
+    Console.WriteLine("You Win a Bugatti!");
 }
+else if (total >= 10)
+{
+    Console.WriteLine("you win a new laptop!");
+}
+else if(total == 7)
+{
+    Console.WriteLine("you win a trip for two!");
+}
+else
+{
+    Console.WriteLine("Sorry, You Lose BASTARD! YOU GET NOTING! HAHAHAHA!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c40f408a90d194879d0d3823bdd5284e3ed2af5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83bc89f72e6e5460942debb84f0e9cbf115d7950")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
