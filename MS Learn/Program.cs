a9f266dc3def0956765fd6ce42efa94c41fd14f0
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2024-01-16 10:12:39 +0100

    Exercise - Perform math operations

--- MS Learn/Program.cs
+++ MS Learn/Program.cs
@@ -169,6 +169,17 @@ string firstName = "Bob";
 int widgetsSold = 7;
 Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
 
+//Exercise - Perform math operations
+
+int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+int quotient = 7 / 5;
+
+Console.WriteLine("sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);
 
 
 
