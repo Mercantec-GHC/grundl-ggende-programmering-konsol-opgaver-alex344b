5423a96c8a94fbb0d7c7ed19500ae3c99eeee173
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2024-01-17 16:44:05 +0100

    update problematic code in the code editor

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e83865aab4da7c91698654ebb024e53066fe5dbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d755992abedec2addd254b1b65d0862856d0327d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
