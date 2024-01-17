dec0cfc2ecd5d444400c12565248dd5723094bf1
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2024-01-17 17:06:31 +0100

    //rewrite if-elseif-else using a switch statement

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -496,6 +496,7 @@ Console.WriteLine($"Total: {total}");
 */
 
 //Create and test a switch statement
+/*
 int employeeLevel = 100;
 string employeeName = "John Smith";
 
@@ -519,5 +520,62 @@ switch (employeeLevel)
 }
 
 Console.WriteLine($"{employeeName}, {title}");
+*/
 
+//rewrite if-elseif-else using a switch statement
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
 
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
+{
+    case "01":
+        type = "Sweat Shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat pants";
+        break;
+    default:
+        type = "other";
+        break;
+}
+
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size fits all!";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5423a96c8a94fbb0d7c7ed19500ae3c99eeee173")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c33455ba7655b0b196fceb2faac3004f985d28fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
