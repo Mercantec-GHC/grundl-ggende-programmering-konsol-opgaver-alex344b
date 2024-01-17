668fab1d03c56e1a50a929eb62e7e3263a9fa573
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2024-01-17 13:51:26 +0100

    // initialize variables - graded assignments, plus ekstra students

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b03071d0664be084480a4bb0f4cd616901da7fc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e841744a3d2083317327bd8390733b4b8b87b008")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
