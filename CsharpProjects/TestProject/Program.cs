c33455ba7655b0b196fceb2faac3004f985d28fd
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2024-01-17 16:52:22 +0100

    Create and test a switch statement

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -475,6 +475,7 @@ else
 */
 
 //update problematic code in the code editor
+/*
 int[] numbers = { 4, 8, 15, 16, 23, 42 };
 int total = 0;
 bool found = false;
@@ -492,3 +493,31 @@ if (found)
     Console.WriteLine("Set contains 42");
 }
 Console.WriteLine($"Total: {total}");
+*/
+
+//Create and test a switch statement
+int employeeLevel = 100;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+    case 100:
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
+        break;
+}
+
+Console.WriteLine($"{employeeName}, {title}");
+
+
