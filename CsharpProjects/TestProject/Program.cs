5c2ac81600706efee202d0a3e80a36d25256f33f
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2024-01-18 14:20:21 +0100

    Write code to reverse each word in a message

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -970,3 +970,26 @@ foreach (string item in items)
     Console.WriteLine(item);
 }
 */
+
+//Write code to reverse each word in a message
+//one way to do it i guess
+// string pangram = "The quick brown fox jumps over the lazy dog";
+// char[] newPangram = pangram.ToCharArray();
+// Array.Reverse(newPangram);
+
+// Console.WriteLine(newPangram);
+
+// another way to do it.
+string pangram = "The quick brown fox jumps over the lazy dog";
+string[] message = pangram.Split(' ');
+string[] newMessage = new string[message.Length];
+for (int i = 0; i < message.Length; i++)
+{
+    char[] newPangram = message[i].ToCharArray();
+    Array.Reverse(newPangram);
+    newMessage[i] = new string(newPangram);
+}
+
+string result = string.Join(" ", newMessage);
+Console.WriteLine(result);
+
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82e02efe1a48567f8c9b9a84b193ba3b2ac5b53e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16a49c04739867285cd30eabed3868c55633492b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
