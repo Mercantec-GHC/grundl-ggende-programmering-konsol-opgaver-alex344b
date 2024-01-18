db3cd3d4a6020268cba74b419bd52d114c881b9e
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2024-01-18 08:40:03 +0100

    MinValue and MaxValue properties for each signed integral type

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ede01141d9c486a7ee71f6866eb5e1a4f43f8803")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6da860caa2ba0f1f3c5a786424f0cc41349a45d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MS Learn/obj/Debug/net6.0/MS Learn.AssemblyInfo.cs
+++ MS Learn/obj/Debug/net6.0/MS Learn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MS Learn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c92676418932717648630a73aefc48801efd0764")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6da860caa2ba0f1f3c5a786424f0cc41349a45d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MS Learn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MS Learn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
